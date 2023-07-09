b723c01725d0c16bda3c575de9457d62170b13a1
Daniel Rodrigues <danieldssr31@gmail.com>
2023-07-09 13:44:58 -0300

    teste

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,8 @@
+describe('Logar no ecommerce', () => {
+    it('Login', () => {
+        cy.visit('https://www.saucedemo.com/v1/')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('#login-button').click()
+    });
+});
