fc3782c1c45a6f75624979f3e5e47117c1eba2ee
Daniel Rodrigues <danieldssr31@gmail.com>
2023-07-09 14:07:08 -0300

    Teste funcional login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,28 @@
-describe('Logar no ecommerce', () => {
-    it('Login', () => {
+/// <reference types="Cypress"/>
+describe('Teste Funcional de Login', () => {
+
+    it('Validar Login com sucesso', () => {
         cy.visit('https://www.saucedemo.com/v1/')
         cy.get('[data-test="username"]').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('#login-button').click()
+        cy.contains('Products').should('be.visible')    
+    });
+
+    it('Validar Login incorreto', () => {
+        cy.visit('https://www.saucedemo.com/v1/')
+        cy.get('[data-test="username"]').type('incorreto')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('#login-button').click() 
+        cy.get('[data-test="error"]').should('be.visible')    
+    });
+
+    it('Validar Senha incorreta', () => {
+        cy.visit('https://www.saucedemo.com/v1/')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('incorreta')
+        cy.get('#login-button').click() 
+        cy.get('[data-test="error"]').should('be.visible')    
     });
 });
+
